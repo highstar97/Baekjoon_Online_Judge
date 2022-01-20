f155d3400d3e7b10156a983c7110cf05c77e486b
highstar97 <pgt0185@gmail.com>
2022-01-20 16:59:40 +0900

    Solve 2750

---
+++ BOJ Tutorial Beta (1)/2750.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+
+int N;
+int arr[1000];
+
+int main()
+{
+    std::ios::sync_with_stdio(0);
+    std::cin.tie(0);
+
+    std::cin >> N;
+
+    for(int i=0; i<N; i++)
+    {
+        std::cin >> arr[i];
+    }
+
+    std::sort(arr,arr+N);
+
+    for(int i=0; i<N; i++)
+    {
+        std::cout << arr[i] << '\n';
+    }
+
+    return 0;
+}
