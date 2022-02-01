c73663ac117a743f9e60a8e2184b1b9948f12d21
highstar97 <pgt0185@gmail.com>
2022-02-01 22:35:00 +0900

    Solve 5430

--- BOJ Tutorial Beta (1)/5430.cpp
+++ BOJ Tutorial Beta (1)/5430.cpp
@@ -1,102 +1,8 @@
 #include <iostream>
-#include <vector>
 #include <deque>
+#include <vector>
 
-int T, n;
-bool isReverse, isError;
-std::string p;
-std::string str_num;
-std::deque<int> dq;
-
-void Initialize()
-{
-    n = 0;
-    isReverse = false;
-    isError = false;
-    p.clear();
-    str_num.clear();
-    dq.clear();
-}
-
-void PushOnlyIntToDeque()
-{
-    std::string temp;
-    for(int i=0; i<str_num.length(); ++i)
-    {
-        if(isdigit(str_num[i]))
-        {
-            temp += str_num[i];
-        }
-        else if(!temp.empty())
-        {
-            dq.push_back(stoi(temp));
-            temp.clear();
-        }
-    }
-}
-
-void Readp()
-{
-    for (int i = 0; i < p.length(); ++i)
-    {
-        switch (p[i])
-        {
-        case ('R'):
-        {
-            isReverse = !isReverse;
-            break;
-        }
-        case ('D'):
-        {
-            if(dq.empty())
-            {
-                isError = true;
-            }
-            else if (!isReverse)
-            {
-                dq.pop_front();
-            }
-            else
-            {
-                dq.pop_back();
-            }
-            break;
-        }
-        }
-    }
-}
-
-void Print()
-{
-    if(isError)
-    {
-        std::cout << "error" << '\n';
-        return ;
-    }
-    else if( n==0 )
-    {
-        std::cout << "[]" << '\n';
-        return ;
-    }
-    std::cout << "[";
-    if(!isReverse)
-    {
-        for(auto it = dq.begin(); it != dq.end()-1; ++it)
-        {
-            std::cout << *it << ",";
-        }
-        std::cout << *(dq.end()-1);
-    }
-    else
-    {
-        for (auto it = dq.rbegin(); it != dq.rend()-1; ++it)
-        {
-            std:: cout << *it << ",";
-        }
-        std::cout << *(dq.rend()-1);
-    }
-    std::cout << "]" << '\n';
-}
+int T;
 
 int main()
 {
@@ -107,17 +13,81 @@ int main()
     std::cin >> T;
     while (T--)
     {
-        Initialize();
+        std::string p;
+        int n;
+        std::string arr;
+        std::deque<int> dq;
 
         std::cin >> p;
         std::cin >> n;
-        std::cin >> str_num;
+        std::cin >> arr;
 
-        PushOnlyIntToDeque();
+        std::string temp;
+        for (int i = 0; i < arr.length(); ++i)
+        {
+            if (isdigit(arr[i]))
+            {
+                temp += arr[i];
+            }
+            else if (!temp.empty())
+            {
+                dq.emplace_back(stoi(temp));
+                temp.clear();
+            }
+        }
 
-        Readp();
+        bool isReverse = false;
+        bool isError = false;
+        for (int i = 0; i < p.length(); ++i)
+        {
+            switch(p[i])
+            {
+            case ('R'):
+            {
+                isReverse = !isReverse;
+                break;
+            }
+            case ('D'):
+            {
+                if (dq.empty())
+                {
+                    isError = true;
+                }
+                else
+                {
+                    isReverse ? dq.pop_back() : dq.pop_front();
+                }
+                break;
+            }
+            }
+        }
 
-        Print();
+        if (isError)
+        {
+            std::cout << "error" << '\n';
+        }
+        else if(dq.size() == 0)
+        {
+            std::cout << "[]\n";
+        }
+        else if (isReverse)
+        {
+            std::cout << "[";
+            for (int i = dq.size() - 1; i > 0; --i)
+            {
+                std::cout << dq[i] << ",";
+            }
+            std::cout << dq[0] << "]\n";
+        }
+        else
+        {
+            std::cout << "[";
+            for (int i = 0; i < dq.size() - 1; ++i)
+            {
+                std::cout << dq[i] << ",";
+            }
+            std::cout << dq[dq.size() - 1] << "]\n";
+        }
     }
     return 0;
 }
