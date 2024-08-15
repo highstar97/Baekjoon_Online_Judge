43425d0e3becfa8734e20319d362b38fe89d5584
highstar97 <pgt0185@gmail.com>
2024-08-15 22:54:52 +0900

    Solve 15652

---
+++ All/15652.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int N, M;
+
+void GeneratePermutations(vector<int> &Vec, int Depth)
+{
+    if (Depth == M)
+    {
+        for (int data : Vec)
+        {
+            cout << data << " ";
+        }
+        cout << '\n';
+        return;
+    }
+
+    for (int i = 1; i <= N; ++i)
+    {
+        if (!Vec.empty() && Vec.back() > i)
+            continue;
+        Vec.push_back(i);
+        GeneratePermutations(Vec, Depth + 1);
+        Vec.pop_back();
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> N >> M;
+    vector<int> Vec;
+    GeneratePermutations(Vec, 0);
+
+    return 0;
+}
