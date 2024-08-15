156dc0a2ddd6025086c531a82bf0adb422a54d40
highstar97 <pgt0185@gmail.com>
2024-08-15 22:59:29 +0900

    Comment 15650

--- All/15650.cpp
+++ All/15650.cpp
@@ -1,3 +1,10 @@
+/*
+    조합 문제.
+
+    Bitmask(11...100...0) + prev_permutaion를 기억하자.
+    Bitmask(00...011...1) + next_permutaion도 가능
+*/
+
 #include <iostream>
 #include <vector>
 #include <algorithm>
